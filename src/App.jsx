7942e6811eb5e3015c9a1d130cc6b0362f2096fb
sharifzademurad <sharifzademurad8@gmail.com>
2026-08-04 11:35:01 +0400

    task 3 tamamlandi

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,21 @@ import { useState, useEffect } from 'react';
 import { SearchBar } from './components/SearchBar';
 import { ResultsList } from './components/ResultsList';
 import { Pagination } from './components/Pagination';
+import { useDebounce } from './hooks/useDebounce';
 
 export function App() {
   const [items, setItems] = useState([]);
   const [searchTerm, setSearchTerm] = useState('');
   const [page, setPage] = useState(1);
-  const totalPages = 5; 
+  const totalPages = 5;
+
+  const debouncedSearchTerm = useDebounce(searchTerm, 500);
 
   useEffect(() => {
-    
     const fetchData = async () => {
       try {
         const response = await fetch(
-          `https://jsonplaceholder.typicode.com/posts?q=${searchTerm}&_page=${page}&_limit=6`
+          `https://jsonplaceholder.typicode.com/posts?q=${debouncedSearchTerm}&_page=${page}&_limit=6`
         );
         const data = await response.json();
         setItems(data);
@@ -24,7 +26,8 @@ export function App() {
     };
 
     fetchData();
-  }, [searchTerm, page]); 
+  }, [debouncedSearchTerm, page]); 
+
   return (
     <div style={{ maxWidth: '700px', margin: '40px auto', padding: '0 20px' }}>
       <h2>API ilə İşləyən Axtarış Tətbiqi</h2>
@@ -33,7 +36,7 @@ export function App() {
         value={searchTerm} 
         onChange={(e) => {
           setSearchTerm(e.target.value);
-          setPage(1); 
+          setPage(1);
         }} 
       />
 
