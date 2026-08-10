7a8247eb86fb40ccd27828f244ba84eb37685143
sharifzademurad <sharifzademurad8@gmail.com>
2026-08-10 12:22:08 +0400

    Change Pagination to default export
    Refactor Pagination component to use default export.

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,4 +1,4 @@
-export const Pagination = ({ currentPage, totalPages, onPageChange }) => {
+const Pagination = ({ currentPage, totalPages, onPageChange }) => {
   if (totalPages <= 1) return null;
 
   return (
@@ -39,3 +39,5 @@ export const Pagination = ({ currentPage, totalPages, onPageChange }) => {
     </div>
   );
 };
+
+export default Pagination;
