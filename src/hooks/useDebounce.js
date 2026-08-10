53bc457b85905b0ca2ceb04e7ec3e57dcc60882e
sharifzademurad <sharifzademurad8@gmail.com>
2026-08-10 12:13:53 +0400

    Refactor useDebounce to default export

--- src/hooks/useDebounce.js
+++ src/hooks/useDebounce.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 
-export const useDebounce = (value, delay = 500) => {
+const useDebounce = (value, delay = 500) => {
   const [debouncedValue, setDebouncedValue] = useState(value);
 
   useEffect(() => {
@@ -15,3 +15,5 @@ export const useDebounce = (value, delay = 500) => {
 
   return debouncedValue;
 };
+
+export default useDebounce;
